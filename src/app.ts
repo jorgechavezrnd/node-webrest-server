9af1999fd5987c65a944f94c3db7c45bf2376cd1
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-08-22 22:05:31 -0400

    Add first example of simple server

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+
+  console.log(req.url);
+
+  res.write('Hola Mundo');
+  res.end();
+
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});

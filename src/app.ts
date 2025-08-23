5b7fee47307b95f43d5bdb76e464888a28c00469
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-08-22 22:30:12 -0400

    Add logic for return response from files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,34 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
 
   console.log(req.url);
 
-  res.write('Hola Mundo');
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(`<h1>URL ${req.url}</h1>`);
+  // res.end();
+
+  // const data = { name: 'John Doe', age: 30, city: 'New York' };
+  // res.writeHead(200, { 'Content-Type': 'application/json' });
+  // res.end(JSON.stringify(data));
+
+  if (req.url === '/') {
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlFile);
+    return;
+  }
+
+  if (req.url?.endsWith('.js')) {
+    res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+    res.end(responseContent);
+  } else if (req.url?.endsWith('.css')) {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8');
+    res.end(responseContent);
+  }
 
 });
 

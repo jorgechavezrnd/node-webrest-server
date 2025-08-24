f63917a400e68859f4be63fcdc2e6cc591447960
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-08-24 09:58:21 -0400

    Add implementation for create todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -27,9 +27,18 @@ export class TodoController {
   };
 
   public createTodo = (req: Request, res: Response) => {
-    const body = req.body;
+    const { text } = req.body;
+    if (!text) return res.status(400).json({ error: 'Text property is required' });
 
-    res.json(body);
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+      createdAt: null,
+    };
+
+    todos.push(newTodo);
+
+    res.json(newTodo);
   };
 
 }

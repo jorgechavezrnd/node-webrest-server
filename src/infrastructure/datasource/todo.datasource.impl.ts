a5433a6236a773b626dbef5dc0630de0d82c5410
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-08-31 10:25:08 -0400

    Add implementation of getAll function of TodoDataSourceImpl

---
+++ src/infrastructure/datasource/todo.datasource.impl.ts
@@ -0,0 +1,27 @@
+import { prisma } from '../../data/postgres';
+import { CreateTodoDto, TodoDataSource, TodoEntity, UpdateTodoDto } from '../../domain';
+
+export class TodoDataSourceImpl implements TodoDataSource {
+
+  create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+    throw new Error('Method not implemented.');
+  }
+
+  async getAll(): Promise<TodoEntity[]> {
+    const todos = await prisma.todo.findMany();
+
+    return todos.map(todo => TodoEntity.fromObject(todo));
+  }
+
+  findById(id: number): Promise<TodoEntity> {
+    throw new Error('Method not implemented.');
+  }
+
+  updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteById(id: number): Promise<TodoEntity> {
+    throw new Error('Method not implemented.');
+  }
+}

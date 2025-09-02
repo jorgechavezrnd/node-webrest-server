640c64a2b6a5bfc0c923f13502c7ef5d974ac5a6
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-02 19:37:31 -0400

    Add tests for app

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,8 +1,22 @@
+import { envs } from '../src/config/envs';
+import { Server } from '../src/presentation/server';
+
+jest.mock('../src/presentation/server');
+
 describe('Testing App.ts', () => {
 
-  test('should work', () => {
+  test('should call server with arguments and start', async () => {
 
-    expect(true).toBeTruthy();
+    await import('../src/app');
+
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      publicPath: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenCalledWith();
 
   });
 

e26b50422cb4aaacc0b004ad111a3a486bdd9830
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-03 14:32:16 -0400

    Add test for get todo by id

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -12,12 +12,14 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   const todo1 = { text: 'Hola mundo 1' };
   const todo2 = { text: 'Hola mundo 2' };
 
   test('should return TODOs api/todos', async () => {
-
-    await prisma.todo.deleteMany();
     await prisma.todo.createMany({
       data: [todo1, todo2],
     });
@@ -31,6 +33,24 @@ describe('Todo route testing', () => {
     expect(body[0].text).toBe(todo1.text);
     expect(body[1].text).toBe(todo2.text);
     expect(body[0].completedAt).toBeUndefined();
+
+  });
+
+  test('should return a TODO api/todos/:id', async () => {
+
+    const todo = await prisma.todo.create({
+      data: todo1,
+    });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${ todo.id }`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo.text,
+      completedAt: undefined,
+    });
 
   });
 

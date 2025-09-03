fe3fd8c9e34ef9af73e01482a693e90d82f87399
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-03 12:02:11 -0400

    Add test for api/todos

--- test/presentation/todos/routes.test.ts
+++ test/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
 
 describe('Todo route testing', () => {
 
@@ -11,13 +12,25 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  const todo1 = { text: 'Hola mundo 1' };
+  const todo2 = { text: 'Hola mundo 2' };
+
   test('should return TODOs api/todos', async () => {
 
-    const response = await request(testServer.app)
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: [todo1, todo2],
+    });
+
+    const { body } = await request(testServer.app)
       .get('/api/todos')
       .expect(200);
 
-    console.log(response.body);
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeUndefined();
 
   });
 
